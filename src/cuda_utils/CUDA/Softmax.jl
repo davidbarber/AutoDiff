278a4d6f1c265f76dc78bb62c272b2fb269a17d3
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-01-25 13:30:00 +0000

    if ADNode present, convolution crashed. Solving the issue

--- src/cuda_utils/CUDA/Softmax.jl
+++ src/cuda_utils/CUDA/Softmax.jl
@@ -8,10 +8,10 @@ const CUDNN_SOFTMAX_MODE_INSTANCE = 0
 const CUDNN_SOFTMAX_MODE_CHANNEL = 1
 
 
-function cudnnSoftmaxForward(handle::cudnnHandle_t,algorithm::Int,mode::Int,alpha,srcDesc::cudnnTensorDescriptor_t,srcData::CudaPtr,beta,destDesc::cudnnTensorDescriptor_t,destData::CudaPtr)
-@cudnncheck(:cudnnSoftmaxForward,(cudnnHandle_t,Cint,Cint,Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void},Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void}),handle,algorithm,mode,Float64[alpha],srcDesc,srcData,Float64[beta],destDesc,destData)
+function cudnnSoftmaxForward{T<:AbstractFloat}(handle::cudnnHandle_t,algorithm::Int,mode::Int,alpha::T,srcDesc::cudnnTensorDescriptor_t,srcData::CudaPtr,beta::T,destDesc::cudnnTensorDescriptor_t,destData::CudaPtr)
+@cudnncheck(:cudnnSoftmaxForward,(cudnnHandle_t,Cint,Cint,Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void},Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void}),handle,algorithm,mode,T[alpha],srcDesc,srcData,T[beta],destDesc,destData)
 end
 
-function cudnnSoftmaxBackward(handle::cudnnHandle_t,algorithm::Int,mode::Int,alpha,srcDesc::cudnnTensorDescriptor_t,srcData::CudaPtr,srcDiffDesc::cudnnTensorDescriptor_t,srcDiffData::CudaPtr,beta,destDiffDesc::cudnnTensorDescriptor_t,destDiffData::CudaPtr)
-@cudnncheck(:cudnnSoftmaxBackward,(cudnnHandle_t,Cint,Cint,Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void},Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void}),handle,algorithm,mode,alpha,srcDesc,srcData.p,srcDiffDesc,srcDiffData.p,beta,destDiffDesc,destDiffData.p)
+function cudnnSoftmaxBackward{T<:AbstractFloat}(handle::cudnnHandle_t,algorithm::Int,mode::Int,alpha::T,srcDesc::cudnnTensorDescriptor_t,srcData::CudaPtr,srcDiffDesc::cudnnTensorDescriptor_t,srcDiffData::CudaPtr,beta::T,destDiffDesc::cudnnTensorDescriptor_t,destDiffData::CudaPtr)
+@cudnncheck(:cudnnSoftmaxBackward,(cudnnHandle_t,Cint,Cint,Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void},Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void}),handle,algorithm,mode,T[alpha],srcDesc,srcData,srcDiffDesc,srcDiffData,T[beta],destDiffDesc,destDiffData)
 end
--- src/functions/FCUsoftmax.jl
+++ src/functions/FCUsoftmax.jl
@@ -9,8 +9,6 @@ function DCUsoftmax()
 println("CPU version of CuDNN softmax under Development")
 end
 
-
-
 function FCUsoftmax(value::CudaArray,au::CudaArray,tensor::CudaTensor)
 dstTensorDesc = cudnnCreateTensorDescriptor()
 dimension = tensor.dims
@@ -22,23 +20,13 @@ end
 export FCUsoftmax
 
 function DCUsoftmax(derivativeIDX,f_c,faux_c,grad_c,grad_n,x::CudaTensor)
-println(typeof(derivativeIDX))
-println(typeof(f_c))
-println(typeof(faux_c))
-println(typeof(grad_c))
-println(typeof(grad_n))
+alpha = 1.0
+beta = 0.0
+cudnnSoftmaxBackward(tensor.handle,1,1,alpha,tensor.tensorDesc,tensor.data.ptr,tensor.tensorDesc,grad_c.ptr,beta,tensor.tensorDesc,grad_n.ptr)
 end
+
 
 Derivative[FCUsoftmax] = DCUsoftmax
 Inplace[FCUsoftmax]   = FCUsoftmax
 CUsoftmax(i::ADTensor)=ADnode(FCUsoftmax,[i])
 export CUsoftmax
-
-
-#TODO: still need to be change to fit ADnode
-function softmaxBackward(inputs::CudaArray,ouputs::CudaArray,diffDesc::cudnnTensorDescriptor_t,diff::CudaArray,n,w,h,c)
-alpha = 1.0
-beta = 0.0
-out = CudaArray(ctx.dataType,n*w*h*c)
-cudnnSoftmaxBackward(ctx.handle,1,1,alpha,ctx.dstTensorDesc,outputs,diffDesc,diff,beta,ctx.srcTensorDesc,out)
-end
